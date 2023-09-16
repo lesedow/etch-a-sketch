d6d512e6548bbdeb9b09d8e366829a63222d8c43
lesedow <lesedow@gmail.com>
2023-09-16 14:50:53 +0300

    Change size of the grid
    Create a slider input, listen for a change event
    and take its new value and modify the size of the grid
    by removing the current tiles and creating new ones
    with the coresponding size.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const gridContainer = document.getElementById('grid-container');
 const root = document.documentElement;
+const sizeRange = document.getElementById('size'); 
+
 let mouseDown = false;
 
 let gridContainerSize = getComputedStyle(root).getPropertyValue('--grid-size');
@@ -25,13 +27,34 @@ function isGridBlock (target) {
     return target.classList.contains('grid-block');
 }
 
+function updateSizeRangeLabel (event) {
+    const sizeRangeValue = event.target.value;
+    const sizeRangeLabel = event.target.previousElementSibling;
+    sizeRangeLabel.textContent = `Size: ${sizeRangeValue} x ${sizeRangeValue}`
+}
+
+function removeCurrentGridTiles() {
+    const gridTiles = gridContainer.querySelectorAll('div');
+    gridTiles.forEach(tile => {
+        gridContainer.removeChild(tile);
+    }); 
+}
+
+function changeGridSize (event) {
+    updateSizeRangeLabel(event);
+    removeCurrentGridTiles();
+    const sizeRangeValue = event.target.value;
+
+    generateGrid(sizeRangeValue);
+}
+
 gridContainer.addEventListener('mousedown', (event) => {
     mouseDown = true
     draw(event)
 });
 gridContainer.addEventListener('mouseup', () => mouseDown = false);
 gridContainer.addEventListener('mouseover', draw);
-
+sizeRange.addEventListener('change', changeGridSize)
 
 
 generateGrid(16);
